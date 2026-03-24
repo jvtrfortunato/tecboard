3081926c9bc22fffd50cf39291893922c970700c
jvtrfortunato <joaovitor.fortunato@hotmail.com>
2026-03-24 17:27:36 -0300

    feat: adiciona componentes ListaSuspensa e Botao no componente FormularioDeEvento

--- src/components/FormularioDeEvento/index.jsx
+++ src/components/FormularioDeEvento/index.jsx
@@ -3,6 +3,8 @@ import { CampoDeEntrada } from "../CampoDeEntrada";
 import { CampoDeFormulario } from "../CampoDeFormulario";
 import { Label } from "../Label";
 import { TituloFormulario } from "../TituloFormulario";
+import { Botao } from '../Botao';
+import { ListaSuspensa } from '../ListaSuspensa';
 
 
 export function FormularioDeEvento() {
@@ -28,6 +30,15 @@ export function FormularioDeEvento() {
             name="dataEvento"
           />
         </CampoDeFormulario>
+        <CampoDeFormulario>
+          <Label htmlFor="dataEvento">Tema do evento</Label>
+          <ListaSuspensa />
+        </CampoDeFormulario>
+      </div>
+      <div className='acoes'>
+        <Botao>
+          Criar evento
+        </Botao>
       </div>
     </form>
   );
