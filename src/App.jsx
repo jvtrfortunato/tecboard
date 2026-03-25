0c96a3c9492145ef2ccd0df88fc6f6c39813f5d7
jvtrfortunato <joaovitor.fortunato@hotmail.com>
2026-03-25 15:01:18 -0300

    feat: cria lista de temas e adiciona componentes Tema e Banner à aplicação

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,48 @@
 import './App.css'
+import { Banner } from './components/Banner'
 import { FormularioDeEvento } from './components/FormularioDeEvento'
+import { Tema } from './components/Tema'
 // no react, componentes são FUNÇÕES
 
 function App() {
+
+  const temas = [
+    {
+      id: 1,
+      nome: 'front-end'
+    },
+    {
+      id: 2,
+      nome: 'back-end'
+    },
+    {
+      id: 3,
+      nome: 'devops'
+    },
+    {
+      id: 4,
+      nome: 'inteligência artificial'
+    },
+    {
+      id: 5,
+      nome: 'data science'
+    },
+    {
+      id: 6,
+      nome: 'cloud'
+    }
+  ]
+
   return (
     <main>
       <header>
         <img src="/logo.png" alt="" />
       </header>
-      <section>
-        <img src="/banner.png" alt="" />
-      </section>
+      <Banner />
       <FormularioDeEvento />
+      <section>
+        <Tema tema={temas[0]}/>
+      </section>
     </main>
   )
 }
